b5cb847f616336e6ee4ced44a3f52754ca141e3c
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 23:56:09 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,3 +1,8 @@
+#     favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #   }
+  # }
+def remove_strawberry(contacts)
+
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
@@ -6,3 +11,19 @@ def remove_strawberry(contacts)
       end
     end
   end
+end
+
+# Alternative solution
+
+def remove_strawberry(contacts)
+  # Assign Freddy Mercury's hash to a variable
+  freddy = contacts["Freddy Mercury"]
+
+  # Iterate over only Freddy Mercury's data
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
+end 
